59fe72e8327e4c72406fc5735a089c275062b8e3
Lanavin <31971824+Lanavin@users.noreply.github.com>
2017-12-14 22:38:16 +0300

    Add files via upload

--- LABA7/lab/admin.py
+++ LABA7/lab/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from .models import Book_User
 # Register your models here.
 
+def upper_case_name(obj):
+    return ("%s" % (obj.id)).upper()
+upper_case_name.short_description = 'id'
+
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('user', 'book', 'number')
+    list_display = ('user', 'book', 'number', upper_case_name)
     list_filter = ('user',)
     search_fields = ['book']
 
